68d39c18f12f9c80130a84fbabbf803da31a2608
Matteo Voltolini <mvoltolini@dominioifs.org>
2025-04-11 07:31:55 +0200

    Fix HTML e funzione sendEmail per invio JSON

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,44 +1,36 @@
 const nodemailer = require("nodemailer");
-const multiparty = require("multiparty");
 
 exports.handler = async (event) => {
-    if (event.httpMethod !== "POST") {
-        return { statusCode: 405, body: "Method Not Allowed" };
-    }
+    try {
+        const formData = JSON.parse(event.body);
 
-    return new Promise((resolve, reject) => {
-        const form = new multiparty.Form();
+        const { emails, mensaje } = formData;
 
-        form.parse(event, async (err, fields, files) => {
-            if (err) {
-                console.error("Error parsing form: ", err);
-                return resolve({
-                    statusCode: 400,
-                    body: JSON.stringify({ error: "Invalid form data" }),
-                });
-            }
+        if (!emails || emails.length === 0) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "No emails provided." }),
+            };
+        }
 
-            try {
-                const emails = fields.emails[0].split(",");
-                const mensaje = fields.mensaje[0];
+        // Configurazione SMTP
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: process.env.SMTP_SECURE === "true",
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS,
+            },
+        });
 
-                // Configurazione SMTP
-                const transporter = nodemailer.createTransport({
-                    host: process.env.SMTP_HOST,
-                    port: process.env.SMTP_PORT,
-                    secure: process.env.SMTP_SECURE === "true",
-                    auth: {
-                        user: process.env.SMTP_USER,
-                        pass: process.env.SMTP_PASS,
-                    },
-                });
-
-                const sendEmailPromises = emails.map((email) => {
-                    return transporter.sendMail({
-                        from: `"Sctrans 👋" <${process.env.SMTP_USER}>`,
-                        to: email,
-                        subject: "Cotización para Sctrans",
-                        text: `Buenas,
+        // Prepara e invia email
+        const sendEmailPromises = emails.map((email) => {
+            return transporter.sendMail({
+                from: `"Sctrans 👋" <${process.env.SMTP_USER}>`,
+                to: email,
+                subject: "Cotización para Sctrans",
+                text: `Buenas,
 
 Estimado colaborador, necesito que me envíen un presupuesto para lo siguiente:
 
@@ -46,27 +38,25 @@ ${mensaje}
 
 Saludos.`,
 
-                        html: `<p>Buenas,</p>
+                html: `<p>Buenas,</p>
 <p>Estimado colaborador, necesito que me envíen un presupuesto para lo siguiente:</p>
 <p>${mensaje}</p>
 <p>Saludos.</p>`,
-                    });
-                });
-
-                await Promise.all(sendEmailPromises);
-
-                return resolve({
-                    statusCode: 200,
-                    body: JSON.stringify({ message: "Emails enviadas correctamente!" }),
-                });
-
-            } catch (error) {
-                console.error("Error al enviar el email:", error);
-                return resolve({
-                    statusCode: 500,
-                    body: JSON.stringify({ error: error.message }),
-                });
-            }
+            });
         });
-    });
+
+        await Promise.all(sendEmailPromises);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Emails enviadas correctamente!" }),
+        };
+
+    } catch (error) {
+        console.error("Error al enviar el email:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
 };
